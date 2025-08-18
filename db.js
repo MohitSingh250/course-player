8c0f10ca4ceff2ced6d073ec1517ffcb612d5072
Mohit Singh <mscrick01@gmail.com>
2025-08-18 12:49:40 +0530

    added Schema connect mongo

---
+++ db.js
@@ -0,0 +1,41 @@
+const {Schema,default:mongoose} = require("mongoose")
+const ObjectId = mongoose.Types.ObjectId
+console.log("mongoose connected")
+mongoose.connect("mongodb+srv://mscrick01:achievemongodb@cluster0.ovfkuhl.mongodb.net/course-player")
+const userSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    password: String,
+    email: {type:String,unique:true}
+})
+
+const adminSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    password: String,
+    email: {type:String,unique:true}
+})
+const courseSchema = new Schema({
+    title:String,
+    description:String,
+    price: Number,
+    imageUrl: String,
+    creatorId: ObjectId
+})
+const purchaseSchema = new Schema({
+    userId: ObjectId,
+    CourseId: ObjectId
+})
+
+const userModel = mongoose.model("user",userSchema)
+const adminModel = mongoose.model("admin",adminSchema)
+const courseModel = mongoose.model("course",courseSchema)
+const purchaseModel = mongoose.model("purchase",purchaseSchema)
+
+module.exports = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+
+}
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const {userRouter} = require("./routes/user.js")
 const {courseRouter} = require("./routes/course.js")
+const {adminRouter} = require("./routes/admin.js")
 
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/course", courseRouter)
